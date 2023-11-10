1704d5314ae3025633ff48d1bab2b38ef33fa59a
nomaro <romano490@hotmail.com>
2023-11-10 08:01:36 +0100

    Update ecoon.py

--- ecoon.py
+++ ecoon.py
@@ -11,8 +11,12 @@ stove = Hottoh('192.168.0.11' ,5001, "lib")
 stove.connect()
 
 attempt = 1
-time.sleep(2)
-stove.set_eco_mode_on()
-print("MODE ECO ON")
-sys.exit()
+
+while True:
+        time.sleep(1)
+        stove.set_eco_mode_on()
+        print("ECO MODE ON")
+        time.sleep(1)
+        stove._getStoveIsOn()
+        sys.exit
 
