e253023332f1e17428b02527a4e782066c973032
nomaro <romano490@hotmail.com>
2023-11-10 08:02:31 +0100

    Update ecooff.py

--- ecooff.py
+++ ecooff.py
@@ -11,8 +11,11 @@ stove = Hottoh('192.168.0.11' ,5001, "lib")
 stove.connect()
 
 attempt = 1
-time.sleep(2)
-stove.set_eco_mode_off()
-print("MODE ECO OFF")
-sys.exit()
 
+while True:
+        time.sleep(1)
+        stove.set_eco_mode_off()
+        print("ECO MODE OFF")
+        time.sleep(1)
+        stove._getStoveIsOn()
+        sys.exit
