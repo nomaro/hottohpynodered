e9dd796afc7cb4840eb5fccf5dc5b9a30252a46c
nomaro <romano490@hotmail.com>
2023-11-11 18:09:46 +0100

    Update infos.py

--- infos.py
+++ infos.py
@@ -1,6 +1,8 @@
+  GNU nano 3.2                                                                                                       infos.py
+
 # importing module
 import sys
-  
+
 # appending a path
 sys.path.append('src')
 from hottohpy import Hottoh
@@ -16,6 +18,7 @@ while True:
     time.sleep(2)
     print("_getStoveState: ", stove._getStoveState())
     print("get_action: ", stove.get_action())
+    print("get_naction: ", stove.get_naction())
     print("_getStoveIsOn: ", stove._getStoveIsOn())
     print("_getEcoMode: ", stove._getEcoMode())
     print("_getSmokeTemperature: ", stove._getSmokeTemperature())
